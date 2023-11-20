edf661708e31315eba53a1ad31d781328ddde206
GabrielGarcia02 <gabriielsimone06@gmail.com>
2023-11-20 20:41:23 -0300

    feature: criada a pagina de cadastro

---
+++ src/screens/Register/styles.ts
@@ -0,0 +1,45 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    background: {
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    container: {
+        height: 400,
+        width: 320,
+        backgroundColor: 'rgba(255,255,255,0.7);',
+        borderRadius: 15,
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 28,
+        paddingTop: 20
+    },
+    input: {
+        color: '#404040',
+        backgroundColor: '#D9D9D9',
+        fontSize: 16,
+        marginTop: 30,
+        padding: 10,
+        borderRadius: 3,
+        marginBottom: 1,
+        height: 40,
+        width: 240,
+    },
+    button: {
+        width: 240,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 16,
+        marginTop: 25,
+        marginBottom: 15,
+        borderRadius: 10,
+        backgroundColor: '#FFFFFF' 
+    },
+    link : {
+        
+    }
+})
